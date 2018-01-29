0ce3283624120d9f50b15a93d3ac3177dd5a2293
Andrew Edmonds <andrewre@umich.edu>
2018-01-29 10:54:22 -0500

    added files for Gemini and Cryptocompare APIs

---
+++ pyalgogem/data/cryptocompare_api.py
@@ -0,0 +1,8 @@
+#
+# PyAlgo Project
+# data/cryptocompare_api
+#
+# functions to interact with Cryptocompare API
+#
+# Andrew Edmonds - 2018
+#
---
+++ pyalgogem/data/gemini_api.py
@@ -0,0 +1,8 @@
+#
+# PyAlgo Project
+# data/gemini_api
+#
+# functions to interact with Gemini API
+#
+# Andrew Edmonds - 2018
+#
