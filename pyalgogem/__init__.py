66b333eda89f2c61069c2c777ac23022e3bd4d6c
Andrew Edmonds <andrewre@umich.edu>
2018-01-29 10:54:49 -0500

    changed package layout so AE is available in main PAG level

--- pyalgogem/__init__.py
+++ pyalgogem/__init__.py
@@ -6,8 +6,4 @@
 # Andrew Edmonds - 2018
 #
 
-from . import base
-from . import data
-from . import backtest
-from . import deployment
-from . import performance
+from .base import AlgorithmEnvironment
