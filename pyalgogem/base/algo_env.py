47dd2410d4794ee8e6f28fb97f20109bccb1cf4e
Andrew Edmonds <andrewre@umich.edu>
2018-02-01 22:15:51 -0500

    edited docstring

--- pyalgogem/base/algo_env.py
+++ pyalgogem/base/algo_env.py
@@ -25,22 +25,22 @@ class AlgorithmEnvironment(object):
 
         Parameters
         ==========
-        key: string
+        key : string
             Gemini API key
-        secret_key: string
+        secret_key:  string
             Gemini secret API key
-        sandbox: bool
+        sandbox : bool
             set to using sandbox account or live account
-        debug: bool
+        debug : bool
             if true, print requests and function outputs
 
         Attributes
         ==========
-        filename: str
+        filename : str
             name of datafile to be used by environment
             on update: creates new HDF5 file in CWD
                 with same name
-        instrument: str
+        instrument : str
             name of instrument to be used - must be BTC or ETH
 
         """
@@ -83,3 +83,4 @@ class AlgorithmEnvironment(object):
     @file.setter
     def file(self, new_file):
         self.__file = data.create_datafile(str(new_file))
+
