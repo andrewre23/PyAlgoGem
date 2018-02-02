620f4134fbf6c7bf8f91c3c30808f9943b8c53c1
Andrew Edmonds <andrewre@umich.edu>
2018-02-01 19:50:50 -0500

    completed class for CryptoCompare API

--- pyalgogem/data/_cryptocompare_api.py
+++ pyalgogem/data/_cryptocompare_api.py
@@ -7,9 +7,65 @@
 # Andrew Edmonds - 2018
 #
 
+import requests
+import datetime as dt
+import pandas as pd
+
+URL_BASE = 'https://min-api.cryptocompare.com/data/'
+
+
 class CryptoCompareAPI(object):
     """Object for housing data retrieval
     functions with CryptoCompare site
     """
-    def get_historical_data(self):
-        pass
+
+    def current_price(symbol, comparison_symbols=['USD'], exchange='Gemini'):
+        """Retrieve current price of currencies"""
+        url = URL_BASE + 'price?fsym={}&tsyms={}' \
+            .format(symbol.upper(), ','.join(comparison_symbols).upper())
+        if exchange:
+            url += '&e={}'.format(exchange)
+        page = requests.get(url)
+        data = page.json()
+        return data
+
+    def historical_price_daily(symbol, comparison_symbol='USD', all_data=True, \
+                               limit=1, aggregate=1, exchange='Gemini'):
+        """Retrieve historical prices by day"""
+        url = URL_BASE + 'histoday?fsym={}&tsym={}&limit={}&aggregate={}' \
+            .format(symbol.upper(), comparison_symbol.upper(), limit, aggregate)
+        if exchange:
+            url += '&e={}'.format(exchange)
+        if all_data:
+            url += '&allData=true'
+        page = requests.get(url)
+        df = pd.DataFrame(page.json()['Data'])
+        df.index = [dt.datetime.fromtimestamp(d) for d in df.time]
+        df = df.drop('time', axis=1)
+        return df
+
+    def historical_price_hourly(symbol, comparison_symbol='USD', limit=1, \
+                                aggregate=1, exchange='Gemini'):
+        """Retrieve historical prices by hour"""
+        url = URL_BASE + 'histohour?fsym={}&tsym={}&limit={}&aggregate={}' \
+            .format(symbol.upper(), comparison_symbol.upper(), limit, aggregate)
+        if exchange:
+            url += '&e={}'.format(exchange)
+        page = requests.get(url)
+        df = pd.DataFrame(page.json()['Data'])
+        df.index = [dt.datetime.fromtimestamp(d) for d in df.time]
+        df = df.drop('time', axis=1)
+        return df
+
+    def historical_price_minute(symbol, comparison_symbol='USD', limit=1, \
+                                aggregate=1, exchange='Gemini'):
+        """Retrieve historical prices by hour"""
+        url = URL_BASE + 'histominute?fsym={}&tsym={}&limit={}&aggregate={}' \
+            .format(symbol.upper(), comparison_symbol.upper(), limit, aggregate)
+        if exchange:
+            url += '&e={}'.format(exchange)
+        page = requests.get(url)
+        df = pd.DataFrame(page.json()['Data'])
+        df.index = [dt.datetime.fromtimestamp(d) for d in df.time]
+        df = df.drop('time', axis=1)
+        return df
