c08fa0fea32d0c5aad77da751da6d7ed0e8885b5
Andrew Edmonds <andrewre@umich.edu>
2018-02-03 14:43:48 -0500

    added module for Gemini Streaming API

--- pyalgogem/data/__init__.py
+++ pyalgogem/data/__init__.py
@@ -11,3 +11,4 @@ from ._cryptocompare_api import CryptoCompareAPI
 from ._data_helpers import ensure_datetime, ensure_hdf5
 from ._file_management import create_datafile, copy_datafile, remove_datafile
 from ._gemini_api import GeminiAPI
+from ._gemini_streamer_api import GeminiStreamAPI
