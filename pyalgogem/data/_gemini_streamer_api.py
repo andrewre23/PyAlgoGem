379387aeef0b83a406e52f06678036642e8b4810
Andrew Edmonds <andrewre@umich.edu>
2018-02-03 14:43:28 -0500

    added module for Gemini Streaming API

---
+++ pyalgogem/data/_gemini_streamer_api.py
@@ -0,0 +1,18 @@
+#
+# PyAlgo Project
+# data/gemini_streamer_api
+#
+# functions to interact with Gemini API
+#
+# Andrew Edmonds - 2018
+#
+
+
+class GeminiStreamAPI(object):
+    """
+    Wrapper class object for Retrieval of Price Data
+    For Cryptocurrencies Using the CryptoCompare API
+    """
+
+    def __init__(self, key, private_key):
+        pass
